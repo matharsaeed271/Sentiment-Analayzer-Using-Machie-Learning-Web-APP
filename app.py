eaaffc5aacf2b61e10b14979e9825af80af9ee6a
Muhammad Athar Ur Rahman <m.atharsaeed271@gmail.com>
2026-03-18 22:12:11 +0500

    Fix indentation in text input handling

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ with st.expander("Analyze Your Text"):
     pre = st.text_input('Clean Your Text: ')
     if pre:
         st.write(cleantext.clean(pre, clean_all= False, extra_spaces=True ,
-                                 stopwords=True ,lowercase=True ,numbers=True , punct=True))
+            stopwords=True ,lowercase=True ,numbers=True , punct=True))
 
 with st.expander('Analyze Excel files'):
     st.write("_**Note**_ : Your file must contain the column Name'Tweets' that contain the text to be analyzed.")
